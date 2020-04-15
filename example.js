729b907d5c3104e743e57dfc72fcd2f11139c81f
Hazel Levine <rose.hazel@protonmail.ch>
2020-04-15 15:02:55 -0400

    initial *attempt* at Wayland support

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 'use strict';
 const clipboardy = require('.');
 
-clipboardy.write('你好🦄');
-clipboardy.read().then(console.log);
+clipboardy.writeSync('你好🦄');
+//clipboardy.read().then(console.log);
